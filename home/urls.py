3f169c09f63ab283cfec3ecff484c8ccd2324900
madhusudhan1557 <sudanm870@gmail.com>
2022-11-08 11:44:18 +0545

    banner fetch

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from home.views import homepage, aboutpage, shop_page, order_form
+from home.views import homepage, aboutpage, shop_page, order_form, contactpage
 
 app_name = "home"
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('index/', homepage, name='homepage'),
     path('about/', aboutpage, name='aboutpage'),
     path('shop/', shop_page, name='shop_page'),
-     path('form/', order_form, name='order_form'),
+    path('form/', order_form, name='order_form'),
+    path('contact/', contactpage, name='contactpage'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from product.models import ProductModel
+from banners.models import BannerModel
 # Create your views here.
 def homepage(request):
     products = ProductModel.objects.all()
-    return render(request, 'home.html', {"products" : products})
+    banners = BannerModel.objects.all()
+    return render(request, 'home.html', {"products" : products, 'banners' : banners})
 
 def aboutpage(request):
-    return render(request, 'about.html')
+    banner = BannerModel.objects.first()
+    return render(request, 'about.html', {"banner" : banner})
 
 def shop_page(request):
     products = ProductModel.objects.all()
-    return render(request, 'shop.html', {"products" : products})
+    banner = BannerModel.objects.last()
+    return render(request, 'shop.html', {"products" : products, "banner" : banner})
     
 def order_form(request):
      return render(request, 'form.html')
+
+def contactpage(request):
+    return render(request, 'contact.html')
