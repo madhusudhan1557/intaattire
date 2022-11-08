f5ff0dae6f6409d638471da0c67482a6b7b312b6
madhusudhan1557 <sudanm870@gmail.com>
2022-11-08 19:27:28 +0545

    url

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from home.views import homepage, aboutpage, shop_page, order_form, contactpage, 
 app_name = "home"
 
 urlpatterns = [
-    path('index/', homepage, name='homepage'),
+    path('', homepage, name='homepage'),
     path('about/', aboutpage, name='aboutpage'),
     path('shop/', shop_page, name='shop_page'),
     path('form/', order_form, name='order_form'),
